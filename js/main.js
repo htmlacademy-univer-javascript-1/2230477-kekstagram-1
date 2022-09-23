cc03264376a41630e251093a9bf55fdcbcdd7b98
paikeee <serzhik2002@gmail.com>
2022-09-23 17:38:41 +0300

    module2-task1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Генерация случайного числа в заданном диапазоне
+function randomNum(start, end) {
+  if (start > end) {
+    return -1;
+  } else if (start == end) {
+    return start;
+  }
+  return Math.floor(Math.random() * (end - start + 1)) + start;
+}
+
+// Проверка длины строки
+function checkString(str, maxLength) {
+    if (str.length <= maxLength) {
+      return true;
+    }
+    return false;
+}
