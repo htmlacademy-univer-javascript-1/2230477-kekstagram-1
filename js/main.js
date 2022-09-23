1587a083c30d86cd530beae742ba25556ee86c7a
paikeee <serzhik2002@gmail.com>
2022-09-23 17:54:12 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 function randomNum(start, end) {
   if (start > end) {
     return -1;
-  } else if (start == end) {
+  } else if (start === end) {
     return start;
   }
   return Math.floor(Math.random() * (end - start + 1)) + start;
@@ -10,8 +10,11 @@ function randomNum(start, end) {
 
 // Проверка длины заданной строки
 function checkString(str, maxLength) {
-    if (str.length <= maxLength) {
-      return true;
-    }
-    return false;
+  if (str.length <= maxLength) {
+    return true;
+  }
+  return false;
 }
+
+console.log(randomNum(1, 10));
+console.log(checkString('abc', 10));
