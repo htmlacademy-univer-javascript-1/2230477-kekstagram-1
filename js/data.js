8f1b4e0fea7dd9e9bfeabeebe928a5afe53bf940
paikeee <serzhik2002@gmail.com>
2022-12-02 12:33:02 +0300

    module5-task1

---
+++ js/data.js
@@ -0,0 +1,37 @@
+import {randomNum} from './util';
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = ['Сергей', 'Мария', 'Иван', 'Аиша', 'Алексей', 'Артем', 'Полина', 'Дарья', 'Юрий'];
+
+const DESCRIPTIONS = ['Ну я', 'Я', 'Это я', 'Ну как я вам'];
+
+const createComment = {
+  id: randomNum(1, 100000000),
+  avatar:  `img/avatar-${randomNum(1, 6)}.svg`,
+  message: MESSAGES[randomNum(1, MESSAGES.length - 1)],
+  name: NAMES[randomNum(1, NAMES.length - 1)]
+};
+
+const createPost = (id) => ({
+  id: id,
+  url: `photos/${id}.jpg`,
+  description: DESCRIPTIONS[randomNum(1, DESCRIPTIONS.length - 1)],
+  likes: randomNum(15, 200),
+  comments: createComment
+});
+
+export const generatePosts = () => {
+  const posts = [];
+  for (let i = 1; i <= 25; i++) {
+    posts.push(createPost(i));
+  }
+  return posts;
+};
--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// Генерация случайного числа в заданном диапазоне
-function randomNum(start, end) {
-  if (start > end) {
-    return -1;
-  } else if (start === end) {
-    return start;
-  }
-  return Math.floor(Math.random() * (end - start + 1)) + start;
-}
+import {generatePosts} from './data';
 
-// Проверка длины заданной строки
-function checkString(str, maxLength) {
-  if (str.length <= maxLength) {
-    return true;
-  }
-  return false;
-}
-
-randomNum(1, 10);
-checkString('abc', 10);
+generatePosts();
